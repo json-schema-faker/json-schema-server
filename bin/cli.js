94b5da41046ea92c905fc2e9864ab6ec1c34b2f0
Alvaro Cabrera <pateketrueke@gmail.com>
2015-08-28 17:17:29 -0500

    Initial impl ready

---
+++ bin/cli.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var pkg = require('../package.json'),
+    utils = require('./lib/utils'),
+    server = require('./lib/server');
+
+var path = require('path'),
+    minimist = require('minimist');
+
+var argv = minimist(process.argv.slice(2), {
+  alias: {
+    h: 'help',
+    v: 'version',
+    f: 'fakeroot',
+    r: 'formats',
+    q: 'silent',
+    p: 'port'
+  },
+  string: ['port', 'formats', 'fakeroot'],
+  boolean: ['help', 'silent', 'version']
+});
+
+function printUsage(message) {
+  if (message) {
+    utils.println(utils.color.red(message + ' (use --help for usage info)'));
+    utils.exit(1);
+  }
+
+  utils.println(utils.style([
+    'Usage: {yellow|' + Object.keys(pkg.bin)[0] + '} [path] {cyan|opts}',
+    '',
+    'Options:',
+    '  {cyan|-p}, {cyan|--port}      {blue|number}   The port used for serving schemas',
+    '  {cyan|-q}, {cyan|--silent}    {blue|boolean}  Disable the output reporting through the STDOUT',
+    '  {cyan|-r}, {cyan|--formats}   {blue|string}   Require CommonJS-module for custom format generators',
+    '  {cyan|-f}, {cyan|--fakeroot}  {blue|string}   Used to resolve $ref\'s from the given directory (optional)',
+    ''
+  ].join('\n')));
+}
+
+
+if (argv.version) {
+  utils.println([pkg.name, pkg.version].join(' '));
+} else if (argv.help) {
+  printUsage();
+} else {
+  var schema_dir = path.resolve(argv._[0] || '.');
+
+  if (!utils.is_dir(schema_dir)) {
+    printUsage('Invalid directory');
+  }
+
+  var formats_src = argv.formats && path.resolve(argv.formats);
+
+  server({
+    port: argv.port,
+    silent: argv.silent,
+    fakeroot: argv.fakeroot,
+    directory: schema_dir,
+    formats: formats_src
+  }, function(err) {
+    if (err) {
+      printUsage(err);
+    } else {
+      utils.println('OK, listening...');
+    }
+  });
+}
---
+++ bin/lib/utils.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var fs = require('fs'),
+    color = require('cli-color');
+
+function print(message) {
+  process.stdout.write(message);
+}
+
+function println(message) {
+  process.stdout.write(message + '\n');
+}
+
+function is_file(filepath) {
+  return fs.existsSync(filepath) && fs.statSync(filepath).isFile();
+}
+
+function is_dir(filepath) {
+  return fs.existsSync(filepath) && fs.statSync(filepath).isDirectory();
+}
+
+function read(filepath) {
+  return fs.readFileSync(filepath);
+}
+
+function style(message) {
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  return message.replace(/\{([.\w]+)\|([^{}]+)\}/g, function() {
+    var colors = arguments[1],
+        msg = arguments[2];
+
+    var colorized = color,
+        segments = colors.split('.');
+
+    while (segments.length) {
+      var key = segments.shift();
+
+      if (!colorized[key]) {
+        break;
+      }
+
+      colorized = colorized[key];
+    }
+
+    return colorized(msg);
+  }).replace(/%s/g, function() {
+    return args.shift();
+  });
+}
+
+module.exports = {
+  exit: process.exit.bind(process),
+  is_file: is_file,
+  is_dir: is_dir,
+  read: read,
+  print: print,
+  println: println,
+  style: style,
+  color: color
+};
